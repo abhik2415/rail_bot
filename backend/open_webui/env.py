bbbe3d32bce3499156a17c1d5c57a82f251b5fe3
Abhik Roy <abhikfiem@gmail.com>
2024-10-30 12:42:42 +0530

    my change

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -99,9 +99,11 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+# WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "विश्वकर्मा मार्गदर्शन")
 if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+    # WEBUI_NAME += " (Open WebUI)"
+    WEBUI_NAME += " (SEALDAH)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
